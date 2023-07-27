c199815228400a7fe664643001a312170d58ea6f
Namne2k3 <nhpn2003@gmail.com>
2023-07-27 22:12:04 +0700

    first commit

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -58,11 +58,11 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-      // frameloop='demand'
-      // shadows
-      // dpr={[1, 2]}
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
-      // gl={{ preserveDrawingBuffer: true }}
+      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
